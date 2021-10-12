67a2819857c8f8ba074ada304fa1b617afe5f624
cb0n3y <cb0n3y@gmail.com>
2021-10-12 11:33:40 +0200

    changed metadata file und classes

--- manifests/systemdunit.pp
+++ manifests/systemdunit.pp
@@ -3,18 +3,18 @@
 # A description of what this class does
 #
 # @example
-#   include dkm_mailcatcher::systemdunit
-class dkm_mailcatcher::systemdunit {
-  file { $idkm_mailcatcher::target_path:
+#   include mailcatcher::systemdunit
+class mailcatcher::systemdunit {
+  file { $mailcatcher::target_path:
     ensure  => present,
-    content => epp('dkm_mailcatcher/mailcatcher.service.epp', {
-      'description'   => $dkm_mailcatcher::service_desc,
-      'documentation' => $dkm_mailcatcher::service_doc,
-      'after'         => $dkm_mailcatcher::service_start,
-      'type'          => $dkm_mailcatcher::service_type,
-      'execstart'     => $dkm_mailcatcher::service_cmd,
-      'restart'       => $dkm_mailcatcher::service_restart,
-      'wantedby'      => $dkm_mailcatcher::service_wanted,
+    content => epp('mailcatcher/mailcatcher.service.epp', {
+      'description'   => $mailcatcher::service_desc,
+      'documentation' => $mailcatcher::service_doc,
+      'after'         => $mailcatcher::service_start,
+      'type'          => $mailcatcher::service_type,
+      'execstart'     => $mailcatcher::service_cmd,
+      'restart'       => $mailcatcher::service_restart,
+      'wantedby'      => $mailcatcher::service_wanted,
     }),
     owner   => 'root',
     group   => 'root',
